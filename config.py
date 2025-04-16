e2de4db4ad354b373469791fb58f41df9e546bbd
widhvans <110589162+widhvans@users.noreply.github.com>
2025-04-16 12:32:28 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,3 +1,5 @@
 # config.py
+API_ID = 27461953  # Replace with your api_id from my.telegram.org (e.g., 1234567)
+API_HASH = "8a19a6a007044ff7b41ada4b377cdfba"  # Replace with your api_hash from my.telegram.org (e.g., "a1b2c3d4e5f6g7h8i9j0")
 BOT_TOKEN = "7953655254:AAGUxu-olhwtUhdjEigPFnQS8T3zoLNhnXs"  # Replace with your bot token from @BotFather
 ADMIN_ID = 1938030055  # Replace with your Telegram user ID (integer)
