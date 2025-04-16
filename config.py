ecbfaf71c8209124c19b32fe043859af085816ad
widhvans <110589162+widhvans@users.noreply.github.com>
2025-04-16 13:12:08 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,3 +3,5 @@ API_ID = 27461953  # Replace with your api_id from my.telegram.org (e.g., 123456
 API_HASH = "8a19a6a007044ff7b41ada4b377cdfba"  # Replace with your api_hash from my.telegram.org (e.g., "a1b2c3d4e5f6g7h8i9j0")
 BOT_TOKEN = "7953655254:AAGUxu-olhwtUhdjEigPFnQS8T3zoLNhnXs"  # Replace with your bot token from @BotFather
 ADMIN_ID = 1938030055  # Replace with your Telegram user ID (integer)
+MONGO_URI = "mongodb+srv://soniji:chaloji@cluster0.i5zy74f.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"  # Replace with your MongoDB connection string (e.g., "mongodb://localhost:27017")
+MONGO_DB_NAME = "lappu"  # Database name for storing chat data
