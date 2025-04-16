948e76974b377aedb2c8703cba4511c73dfb5343
widhvans <110589162+widhvans@users.noreply.github.com>
2025-04-16 12:26:15 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,3 +1,3 @@
 # config.py
-BOT_TOKEN = "your_bot_token_here"  # Replace with your bot token from @BotFather
-ADMIN_ID = your_admin_id  # Replace with your Telegram user ID (integer)
+BOT_TOKEN = "7953655254:AAGUxu-olhwtUhdjEigPFnQS8T3zoLNhnXs"  # Replace with your bot token from @BotFather
+ADMIN_ID = 1938030055  # Replace with your Telegram user ID (integer)
